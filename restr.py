34178aefd2e3bb5dd7333add7e1d31c92abb09eb
NicatAsk99 <asknicat050@gmail.com>
2026-01-19 01:20:34 +0400

    ikinci

--- restr.py
+++ restr.py
@@ -5,7 +5,9 @@ try:
 
         '1 - Salatlar':'1-Toyuq salati 3 azn\n 2 -Gobelek salati 4 azn\n 3 -Sezar salat 7 azn ',
         '2 - Şorbalar':'Bors 4 azn\nMerci3azn',
-        '3 - Soyuq Qəlyanaltı':'Ağ pendir 3 azn\nsuzme 3 azn\nbilincikq 1.50'
+        '3 - Soyuq Qəlyanaltı':'Ağ pendir 3 azn\nsuzme 3 azn\nbilincikq 1.50',
+        '4 - Milli Yeməklər': '1.Hisə vərilmiş dana qabırğası 18 az\n2. Quzu antrikotu baby kartof ilə 21azn\n3. Dana antrikot tərəvəz ilə 18azn\n'
+        
       
         
     
@@ -30,22 +32,21 @@ try:
 
 
 
-
-
-
-
-
-
-
-
-
-
         elif s2=='2':
             print(menu['2 - Şorbalar'])
             input('davam etmek ucun ENTER basin')
 
         elif s2=='3':
-            print(menu)
+            print(menu['3 - Soyuq Qəlyanaltı'])
+            input('davam etmek ucun ENTER basin')
+
+        elif s2=='4':
+            print(menu['4 - Milli Yeməklər'])
+            input('davam etmek ucun ENTER basin')
+
+
+
+
 
 
 
